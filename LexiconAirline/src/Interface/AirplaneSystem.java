e6f9064dce361491c86df65c2e6ab4f81401c93e
chiblo <bardia_swa@yahoo.com>
2016-07-11 13:58:04 +0200

    Bardia

--- LexiconAirline/src/Interface/AirplaneSystem.java
+++ LexiconAirline/src/Interface/AirplaneSystem.java
@@ -3,7 +3,17 @@ package Interface;
 /**
  * Created by User on 2016-07-11.
  */
-public class AirplaneSystem {
-//    Airport GetDepartureAirport();
-//    Airport Get
+public interface AirplaneSystem {
+    Airport getDstinationAirport();
+
+    Airport getDepartureAirport();
+
+    int timeOfArrival();
+
+    PlaneStatus planeStatus();
+
+    int numberEmptySeatsEconomy();
+
+    int numberEmptySeatsFirstClass();
+
 }
--- LexiconAirline/src/Interface/PassengerSystem.java
+++ LexiconAirline/src/Interface/PassengerSystem.java
@@ -4,7 +4,9 @@ package Interface;
  * Created by User on 2016-07-11.
  */
 public interface PassengerSystem {
-//    Flight getPassengerFlight();
+    Flight getPassengerFlight();
+
     int getPassengerTimeOfDeparcure();
+
     int getPassengerTimeOfArrival();
 }
