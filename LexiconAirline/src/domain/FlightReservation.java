0fcaf0878b90a1ff70bfa537135073086325c90a
olegggold <oleggold@hotmail.com>
2016-07-11 15:35:15 +0200

    Updated FlightReservation with two new fields

--- LexiconAirline/src/domain/FlightReservation.java
+++ LexiconAirline/src/domain/FlightReservation.java
@@ -7,11 +7,15 @@ public class FlightReservation {
 		private Passenger passenger;
 		private Plane plane;
 		private Meal meal;
+		private String reservationNumber;
+		private int seatNumber;
 		
-		public FlightReservation(Passenger passenger, Plane plane, Meal meal) {
+		public FlightReservation(Passenger passenger, Plane plane, Meal meal, String reservationNumber, int seatNumber) {
 			this.passenger = passenger;
 			this.plane = plane;
 			this.meal = meal;
+			this.reservationNumber = reservationNumber;
+			this.seatNumber = seatNumber;
 		}
 
 		public Passenger getPassenger() {
@@ -36,6 +40,22 @@ public class FlightReservation {
 
 		public void setMeal(Meal meal) {
 			this.meal = meal;
+		}
+
+		public String getReservationNumber() {
+			return reservationNumber;
+		}
+
+		public void setReservationNumber(String reservationNumber) {
+			this.reservationNumber = reservationNumber;
+		}
+
+		public int getSeatNumber() {
+			return seatNumber;
+		}
+
+		public void setSeatNumber(int seatNumber) {
+			this.seatNumber = seatNumber;
 		}
 	
 	
